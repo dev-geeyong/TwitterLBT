145e375061793294936df1547160db310e4535da
dev-geeyong <dev.geeyong@gmail.com>
2021-03-03 19:10:35 +0900

    메인 뷰 UI 구현

--- TwitterLBT/AppDelegate.swift
+++ TwitterLBT/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //스토리보드 해제
+        
         return true
     }
 
---
+++ TwitterLBT/HomeController.swift
@@ -0,0 +1,85 @@
+//
+//  HomeController.swift
+//  TwitterLBT
+//
+//  Created by dev.geeyong on 2021/03/03.
+//
+
+import UIKit
+
+class ContentCell: UICollectionViewCell{
+    
+    let textLabel:UILabel = {
+        let label = UILabel()
+        label.text = "test , test "
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        
+    }
+    func setupViews(){
+        backgroundColor = .white
+        addSubview(textLabel)
+        textLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        textLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        textLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        textLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class HomeController: UICollectionViewController,UICollectionViewDelegateFlowLayout{
+    let cellId = "cellId"
+    let headerId = "headerId"
+    let footerId = "footerId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        collectionView.register(ContentCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
+        collectionView.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: footerId)
+        
+    }
+    //MARK: - header , footer
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader{
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+            header.backgroundColor = .yellow
+            return header
+        }else{
+            let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: footerId, for: indexPath)
+            footer.backgroundColor = .green
+            return footer
+        }
+        
+    }
+     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 80)
+    }
+     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+    
+    
+    //MARK: - contents
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+}
